823fedbd92a3849097f48f628effd9336185180a
Luiz Gonzaga <103431918+LuizGonzagaDev@users.noreply.github.com>
2024-10-01 12:45:30 -0300

    extraindo os dados do arquivo

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
+from docx import Document
 
 #entra no site 
 driver = webdriver.Chrome()
@@ -31,5 +32,37 @@ sleep(3)
 botoes_sistemas = driver.find_elements(By.XPATH,"//a[@class='btn btn-primary mt-auto']")
 sleep(2)
 botoes_sistemas[0].click()
+
+# extraindo os dados do arquivo word
+caminho_arquivo_word = r'C:\Users\Luiz\Documents\GitHub\RemotoContabilidade\relatorios\Relatorio_Contabilidade_Delícias_Foodie.docx'
+arquivo_word = Document(caminho_arquivo_word)
+
+ativo_circulante = ''
+caixa_equivalente = ''
+contas_receber = ''
+estoques = ''
+ativo_nao_circulante = ''
+imobilizado = ''
+intangivel = ''
+total_ativo = ''
+
+for tabela in arquivo_word.tables:
+    for linha in tabela.rows:
+        if 'Ativo Circulante' in linha.cells[0].text.strip():
+            ativo_circulante = linha.cells[1].text.strip()
+        elif 'Caixa e Equivalentes' in linha.cells[0].text.strip():
+            caixa_equivalente = linha.cells[1].text.strip()
+        elif 'Contas a Receber' in linha.cells[0].text.strip():
+            contas_receber = linha.cells[1].text.strip()
+        elif 'Estoques' in linha.cells[0].text.strip():
+            estoques= linha.cells[1].text.strip()
+        elif 'Ativo Não Circulante' in linha.cells[0].text.strip():
+            ativo_nao_circulante= linha.cells[1].text.strip()
+        elif 'Imobilizado' in linha.cells[0].text.strip():
+            imobilizado= linha.cells[1].text.strip()
+        elif 'Intangível' in linha.cells[0].text.strip():
+            intangivel= linha.cells[1].text.strip()
+        elif 'Total do Ativo' in linha.cells[0].text.strip():
+            total_ativo= linha.cells[1].text.strip()
 
 input('aperte ENTER para fechar')
