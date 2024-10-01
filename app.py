cd59f29bd9c1e3c246493bbb1d9f0b72579b7cd3
Luiz Gonzaga <103431918+LuizGonzagaDev@users.noreply.github.com>
2024-10-01 11:30:00 -0300

    preenche email, senha e entrar

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from time import sleep
 
 #entra no site 
 driver = webdriver.Chrome()
 driver.get('https://contabil-devaprender.netlify.app/')
 
-driver.find_element(By.XPATH,"//input[@type='email']")
+#preenche email, senha e entrar
+
+#campo email
+campo_email = driver.find_element(By.XPATH,"//input[@type='email']")
+sleep(2)
+campo_email.click()
+campo_email.send_keys('emailfake@hotmail.com')
+sleep(2)
+
+#campo senha
+campo_senha = driver.find_element(By.XPATH,"//input[@type='password']")
+sleep(2)
+campo_senha.click()
+campo_senha.send_keys('123456')
+sleep(2)
+
+#botao entrar
+botao_entrar = driver.find_element(By.XPATH,"//button[@class='btn btn-primary w-100']")
+botao_entrar.click()
+
 
 
 input('aperte ENTER para fechar')
